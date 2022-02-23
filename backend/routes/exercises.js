7dcefee76456dcdeb6ad7ea98b89af96552deae6
Jonathan Ong <30916534+jonathankong@users.noreply.github.com>
2022-02-22 23:17:12 -0500

    Update exercises.js
    Added more routes

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -17,7 +17,34 @@ router.post('/add', ash(async (req, res) => {
         duration: req.body.duration,
         date: req.body.date
     });
-    res.json('Exercise added!');
+    res.send('Exercise added!');
+}));
+
+router.get('/:id', ash(async (req, res) => {
+    res.json(await Exercise.findById(req.params.id));
+}));
+
+router.delete('/:id', ash(async (req, res) => {
+    const exercise = await Exercise.findByIdAndDelete(req.params.id);
+    if (exercise !== null) {
+        res.send(`Exercise ${exercise.id} has been deleted!`);
+    } else {
+        res.send(`Couldn't find exercise by id ${req.params.id}`);
+    }
+}));
+
+router.post('/update/:id', ash(async (req, res) => {
+    let exercise = await Exercise.findById(req.params.id);
+    if (exercise !== null) {
+        exercise.username = req.body.username;
+        exercise.description = req.body.description;
+        exercise.duration = req.body.duration;
+        exercise.date = req.body.date;
+        await exercise.save();
+        res.send('Exercise updated!');
+    } else {
+        res.send(`Couldn't find exercise by id ${req.params.id}`);
+    }
 }));
 
 module.exports = router;
